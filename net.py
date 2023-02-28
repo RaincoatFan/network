7e7cb2c76a8e2247bcbf7ddc8fc5ad90dccdbc83
Administrator <zhangbc0224@163.com>
2023-02-28 16:35:59 +0800

    通过csv文件中的坐标点以及txt文件中的关系边绘制出net，包括权重。

--- net.py
+++ net.py
@@ -1,16 +1,67 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import csv
 
-G = nx.Graph()
+def main():
+    G = nx.Graph()
+    # 添加对应的边和点
+    id_tag = []
+    sort_tag = []
+    x_tag = []
+    y_tag = []
+    with open('stops.csv', encoding='utf-8') as f:
+        for row in csv.reader(f, skipinitialspace=True):
+            id_tag.append(row[0])
+            sort_tag.append(row[1])
+            x_tag.append(row[2])
+            y_tag.append(row[3])
+        del id_tag[0], sort_tag[0], x_tag[0], y_tag[0]
 
-# G = nx.cubical_graph()
+    id_list = []    #点
+    x_list = [] #x坐标
+    y_list = [] #y坐标
+    for item in id_tag:
+        id_list.append(int(item))
+    for item in x_tag:
+        x_list.append(int(item))
+    for item in y_tag:
+        y_list.append(int(item))
 
-# subax2 = plt.subplot(122)
-# nx.draw(G, pos=nx.circular_layout(G), node_color='r', edge_color='b')
+    coor_list = []  #将x,y组合成坐标
+    for item in range(len(x_list)):
+        coor = (x_list[item],y_list[item])
+        coor_list.append(coor)
 
-e = [('a', 'b', 0.3), ('b', 'c', 0.9), ('a', 'c', 0.5), ('c', 'd', 1.2)]
-G.add_weighted_edges_from(e)
-print(nx.dijkstra_path(G, 'a', 'd'))
+    for item in id_list:
+        G.add_node(item, desc=str(item))
 
-subax1 = plt.subplot(121)
-nx.draw(G)   # default spring_layout
+    edge = []   #边
+    with open('edge-8.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            line = tuple(line.replace('\r', '').replace('\n', '').replace('\t', '').split(','))
+            edge.append(line)
+
+    distance = []   #距离集合
+    for item in range(len(edge)):
+        s = pow(pow(x_list[int(edge[item][0])]-x_list[int(edge[item][1])],2)+pow(y_list[int(edge[item][0])]-y_list[int(edge[item][1])],2),0.5)
+        s = round(s, 2)
+        distance.append(s)
+
+    for item in range(len(edge)):
+        G.add_edge(int(edge[item][0]), int(edge[item][1]), name=distance[item])
+
+    #画图
+    pos = coor_list
+    # 按pos所定位置画出节点,无标签无权值
+    nx.draw_networkx(G, pos, with_labels=None)
+    # 画出标签
+    node_labels = nx.get_node_attributes(G, 'desc')
+    nx.draw_networkx_labels(G, pos, labels=node_labels)
+    # 画出边权值
+    edge_labels = nx.get_edge_attributes(G, 'name')
+    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels)
+    plt.title('Net', fontsize=10)
+    plt.show()
+
+if __name__ == '__main__':
+    main()
